4e926de51b933e524c193315da9e0a68a023b649
robbryandev <robbryandev@gmail.com>
2023-06-25 14:38:13 -0700

    Minimal app generation

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,23 +1,98 @@
 import Generator from 'yeoman-generator';
+import { join } from "path"
+import { copySync } from 'fs-extra';
+import { chdir, cwd } from 'process';
+import term from "cli-color";
+import { input } from '@inquirer/prompts';
+import { readFileSync, writeFileSync } from 'fs';
+import { exec } from 'child_process';
 
 type BaseArgs = {
-    name: string
+  name: string
+}
+
+type CustomProps = {
+  name: string,
+  appId: string
+}
+
+async function customizePrompts(name: string) {
+  const appId = await input({ message: term.magenta("Enter your app id (Format example: com.example.www):") })
+  const props: CustomProps = {
+    name: name,
+    appId: appId
+  }
+  return props
 }
 
 module.exports = class extends Generator {
-    constructor(args: string | string[], opts: BaseArgs) {
-      super(args, opts);
-  
-      this.argument('name', {
-        required: true,
-        type: String,
-        description: 'The name of the thing',
-      });
+  constructor(args: string | string[], opts: BaseArgs) {
+    super(args, opts, { customInstallTask: true });
 
-    }
-
-    method1() {
-        this.log(this.options.name)
-        this.log('method 1 just ran');
-    }
+    this.argument('name', {
+      required: false,
+      type: String,
+      default: "nativeforgeApp",
+      description: 'The name of your app',
+    });
   }
+
+  start() {
+    if (this.options.name === "nativeforgeApp") {
+      this.log(term.yellow(`
+Using default app name: nativeforgeApp
+      to change the generated app name make your app with:
+        yo nativeforge your_app_name
+`))
+    }
+    this.log(`Generating NativeForge app: ${this.options.name}`);
+  }
+
+  copyFolder() {
+    const basePath = join(__dirname, '/template');
+    copySync(basePath, `${cwd()}/${this.options.name}`)
+    this.log(term.green("Copied base project\n"))
+  }
+
+  install() {
+    this.log(term.blue.bold.underline("Welcome to the NativeForge setup\n"))
+    const appPath = `${cwd()}/${this.options.name}`
+    customizePrompts(this.options.name).then((props: CustomProps) => {
+      this.log(props)
+
+      /* Prompts to replace:
+        $appName
+        $appID
+      */
+
+      const readme = `${appPath}/readme.md`
+      const newReadme = readFileSync(readme, "utf-8")
+        .replace("$appName", props.name);
+      writeFileSync(readme, newReadme)
+
+      const packages = `${appPath}/package.json`
+      const newPackages = readFileSync(packages, "utf-8")
+        .replace(/\$appName/g, props.name);
+      writeFileSync(packages, newPackages)
+
+      const appJson = `${appPath}/apps/expo/app.json`
+      const newAppJson = readFileSync(appJson, "utf-8")
+        .replace(/\$appName/g, props.name)
+        .replace(/\$appID/g, props.appId);
+      writeFileSync(appJson, newAppJson)
+      this.log(term.green("Updated expo settings"))
+      this.log(term.blue("Installing dependencies"))
+      chdir(appPath)
+      exec("yarn install", (err, std) => {
+        if (err) {
+          this.log(term.red("Installation failed"))
+          this.log(err)
+        } else {
+          this.log(std)
+        }
+        this.log(term.green("Installed dependencies"))
+        this.log(term.blue("Thanks for using NativeForge"))
+      })
+    })
+  }
+}
--- src/app/template/readme.md
+++ src/app/template/readme.md
@@ -1,18 +1,7 @@
-# Solito + NativeWind Example Monorepo 🕴
+# $appName
 
-```sh
-npx create-solito-app@latest my-solito-app -t with-tailwind
-```
-
-And just like that, you now have an Expo + Next.js app that is styled with Tailwind CSS.
-
-## ⚡️ Instantly clone & deploy
-
-[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fnandorojo%2Fsolito%2Ftree%2Fmaster%2Fexample-monorepos%2Fwith-tailwind&root-directory=apps/next&envDescription=Set%20this%20environment%20variable%20to%201%20for%20Turborepo%20to%20cache%20your%20node_modules.&envLink=https%3A%2F%2Ftwitter.com%2Fjaredpalmer%2Fstatus%2F1488954563533189124&project-name=solito-app&repo-name=solito-app&demo-title=Solito%20App%20%E2%9A%A1%EF%B8%8F&demo-description=React%20Native%20%2B%20Next.js%20starter%20with%20Solito.%20Made%20by%20Fernando%20Rojo.&demo-url=https%3A%2F%2Fsolito.dev%2Fstarter&demo-image=https%3A%2F%2Fsolito.dev%2Fimg%2Fog.png&build-command=cd+..%2F..%3Bnpx+turbo+run+build+--filter%3Dnext-app)
-
-## 🔦 About
-
-This monorepo is a starter for an Expo + Next.js app using [NativeWind](https://nativewind.dev) for its styling & [Solito](https://solito.dev) for navigation.
+## What is NativeForge?
+NativeForge is a bootsrapping framework for building cross platform apps using [solito](https://solito.dev) and feature generators
 
 ## 👓 How NativeWind works with Solito
 
