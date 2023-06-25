9ef92497aec2c29c272ac38e521f78c1384707ee
robbryandev <robbryandev@gmail.com>
2023-06-25 00:41:43 -0700

    Add base generator

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,1 +1,15 @@
-console.log("test")
+import Generator from 'yeoman-generator';
+
+class NativeForgeGenerator extends Generator {
+    constructor(args: any, opts: any) {
+      super(args, opts);
+  
+      this.argument('name', {
+        required: true,
+        type: String,
+        description: 'The name of the thing',
+      });
+    }
+  }
+  
+  module.exports = NativeForgeGenerator;
